bafc838b4917fced058fedfe8a8d3fa87256d499
Mark Kikta <mark.jr@kikta.us>
2025-02-04 15:38:29 -0500

    Add llm

--- web/server/src/model.ts
+++ web/server/src/model.ts
@@ -1,1 +1,22 @@
-// All LLM calls and types should go here
+import { HfInference } from "@huggingface/inference";
+
+// access code should be stored in proxy server for front-end protection
+const accessCode = "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";
+
+const hf = new HfInference(accessCode);
+
+const infer = async() => {
+    const chatCompletion = await hf.chatCompletion({
+        model: "deepseek-ai/DeepSeek-R1",
+        messages: [
+            {
+                role: "user",
+                content: "What is the capital of France?"
+            }
+        ],
+        provider: "together",
+        max_tokens: 500
+    });
+
+    console.log(chatCompletion.choices[0].message);
+}
