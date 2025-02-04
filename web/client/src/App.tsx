1d4df577e2bc82bd9d40199ae83c688220563699
Spencer Lee <spencer.hayes.lee@gmail.com>
2025-02-04 14:23:39 -0500

    Introduce Material-UI

--- web/client/src/App.tsx
+++ web/client/src/App.tsx
@@ -1,41 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { sendMockRequest } from './requests/playRequest'
+import { Box, Container, CssBaseline, Stack, ThemeProvider } from '@mui/material'
+import theme from './theme/theme'
+import Form from './components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button onClick={async () => {
-        const data = await sendMockRequest();
-        console.log(data);
-      }}>
-        Send mock request
-      </button>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Box height="100vh" display={"flex"} flexDirection={"column"}>
+          <Stack direction="row" spacing={0} alignItems="stretch" flex={1}>
+            <Box flex={1} paddingTop={3} paddingBottom={3}>
+              <Form />
+            </Box>
+            <Box flex={1} paddingTop={3} paddingBottom={3}>
+              Test
+            </Box>
+          </Stack>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
 
