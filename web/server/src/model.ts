8d3e0775225f939a562728ef84ff9e497915f98f
Mark Kikta <mark.jr@kikta.us>
2025-02-11 15:32:53 -0500

    Implement ollama model

--- web/server/src/model.ts
+++ web/server/src/model.ts
@@ -1,22 +1,25 @@
-import { HfInference } from "@huggingface/inference";
+import ollama, { Message as Msg } from 'ollama'
 
-// access code should be stored in proxy server for front-end protection
-const accessCode = "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";
+class Message implements Msg {
+    role: string;
+    content: string;
+    constructor(role: string, content: string) {
+        this.role = role
+        this.content = content
+    }
+}
 
-const hf = new HfInference(accessCode);
-
-const infer = async() => {
-    const chatCompletion = await hf.chatCompletion({
-        model: "deepseek-ai/DeepSeek-R1",
-        messages: [
-            {
-                role: "user",
-                content: "What is the capital of France?"
-            }
-        ],
-        provider: "together",
-        max_tokens: 500
+// Given a user question and a list of context, return the model's response.
+async function completeChat(question: string, context: [string], model: string = 'deepseek-r1:1.5b') {
+    var messages: [Message];
+    messages = [new Message("system", 'You are a coaching assistant for an NFL team. You are very knowledgeable on NFL players, plays, and game histories. You respond with helpful and CONCISE suggestions to users inquiring about the outcome of plays.')]
+    messages.push(new Message('user', question))
+    for (const c of context) {
+        messages.push(new Message('assistant', c))
+    }
+    const response = await ollama.chat({
+        model: model,
+        messages: messages
     });
-
-    console.log(chatCompletion.choices[0].message);
+    return response.message.content;
 }
